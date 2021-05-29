981563f0e145b45026c5fed5e9f95814205b4cc2
blushful8 <84133431+blushful8@users.noreply.github.com>
2021-05-29 19:40:35 +0300

    Commit

---
+++ app/src/main/java/com/example/schedule/Schedule/FridaySecond.java
@@ -0,0 +1,65 @@
+package com.example.schedule.Schedule;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.schedule.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FridaySecond#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FridaySecond extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FridaySecond() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FridaySecond.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FridaySecond newInstance(String param1, String param2) {
+        FridaySecond fragment = new FridaySecond();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friday_second, container, false);
+    }
+}
