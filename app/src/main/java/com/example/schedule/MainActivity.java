6a46f18ebe405d7a50c9f0f1b34296cbb5c76433
KontVIP <76660306+KontVIP@users.noreply.github.com>
2021-05-29 16:18:39 +0300

    Main menu 2

--- app/src/main/java/com/example/schedule/MainActivity.java
+++ app/src/main/java/com/example/schedule/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.schedule;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -27,11 +26,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         init();
-        Log.d("deb", userId + "LOLOLO");
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
