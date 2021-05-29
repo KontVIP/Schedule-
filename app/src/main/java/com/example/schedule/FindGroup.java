848814f14215e595e537c36762c09ed64b8a4c80
blushful8 <84133431+blushful8@users.noreply.github.com>
2021-05-29 17:49:49 +0300

    Commit

---
+++ app/src/main/java/com/example/schedule/FindGroup.java
@@ -0,0 +1,14 @@
+package com.example.schedule;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FindGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_group);
+    }
+}
