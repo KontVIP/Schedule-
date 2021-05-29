e8dbab5799a00f5eb9cb0bd9dab86eded735e38a
KontVIP <76660306+KontVIP@users.noreply.github.com>
2021-05-29 17:01:30 +0300

    ...

--- app/src/main/java/com/example/schedule/MainActivity.java
+++ app/src/main/java/com/example/schedule/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.schedule;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.schedule.Auth.User;
 import com.example.schedule.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,12 +25,15 @@ import com.google.firebase.database.ValueEventListener;
 
 import io.paperdb.Paper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView fullNameTextView, emailTextView, studentTextView;
     private DatabaseReference databaseReference;
     private User user;
     String userId;
+    Button leaveButton;
+
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +57,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        leaveButton.setOnClickListener(this);
+        emailTextView.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_leave:
+                Paper.book().destroy();
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, com.example.schedule.Auth.AuthActivity.class));
+                break;
+            case R.id.email_edit_text:
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("Email", user.email);
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(this, "Скопійовано", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
     private void init() {
+        leaveButton = findViewById(R.id.btn_leave);
         userId = Paper.book().read("UserId");
         emailTextView = findViewById(R.id.email_edit_text);
         fullNameTextView = findViewById(R.id.tv_name);
@@ -57,4 +88,30 @@ public class MainActivity extends AppCompatActivity {
         databaseReference = FirebaseDatabase.getInstance().getReference("Users");
 
     }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            this.finishAffinity();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Натисніть НАЗАД ще раз для виходу", Toast.LENGTH_SHORT).show();
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                doubleBackToExitPressedOnce = false;
+            }
+        };
+        thread.start();
+
+    }
+
 }
