45245ffac2d55655042a3206a72f7ea400d86244
KontVIP <76660306+KontVIP@users.noreply.github.com>
2021-05-29 12:15:08 +0300

    Added new Activities for auth

--- app/src/main/java/com/example/schedule/Auth/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/schedule/Auth/ForgotPasswordActivity.java
@@ -1,4 +1,87 @@
 package com.example.schedule.Auth;
 
-public class ForgotPasswordActivity {
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.schedule.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private Button resetPasswordButton;
+    private EditText emailEditText;
+    private ProgressBar progressBar;
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        emailEditText = (EditText) findViewById(R.id.email_edit_text);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        auth = FirebaseAuth.getInstance();
+
+
+        resetPasswordButton = (Button) findViewById(R.id.reset_password_button);
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+        });
+    }
+
+    private void resetPassword() {
+        String email = emailEditText.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            emailEditText.setError("Необхідний email!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Введіть коректний email!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if(task.isSuccessful()) {
+                    Toast.makeText(ForgotPasswordActivity.this, "Деталі надіслано на пошту!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ForgotPasswordActivity.this, "Помилка! Спробуйте ще раз!", Toast.LENGTH_SHORT).show();
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+    }
 }
+
+
+
+
+
+
+
+
+
